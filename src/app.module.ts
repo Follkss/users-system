be6170bcc90cd8c14a568d08aa9d6fa44e462ecf
André Gosling Dias <goslingdbusiness@gmail.com>
2022-11-26 15:09:27 -0300

    update: remove static code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { typeormConfig } from './config/drivers';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeormConfig), UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
